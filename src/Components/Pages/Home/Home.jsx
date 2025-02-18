db28db7ec96a474d806653f4cc4902dbe8401487
Amirali Feizi <frontify.dev@gmail.com>
2025-02-18 21:43:25 +0330

    feat: add images, custom fonts, install X icon, and configure colors

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -1,7 +1,12 @@
+import { Heart } from 'iconsax-react'
 import React from 'react'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+<Heart size="32" color="#FF8A65"/> <span className='font-IRANSansXRegular'>آیکون ها اضافه شد</span>
+      <div className='font-IRANSansXBold text-Neutral-100 bg-Primary-Pressed'>فونت ایران سانس ایکس اضافه شد</div>
+      <span className='bg-Semantic-Warning'>رنگ ها اضافه شد</span>
+    </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,44 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        extend: {
+            fontFamily: {
+                IRANSansX: 'IranSansX',
+                IRANSansXBold: 'IRANSansX-Bold',
+                IRANSansXDemiBold: 'IRANSansX-DemiBold',
+                IRANSansXRegular: 'IRANSansX-Regular',
+            },
+        },
+        colors: {
+            Neutral: {
+                10: '#1A1A1A',
+                20: '#333333',
+                30: '#4D4D4D',
+                40: '#666666',
+                50: '#808080',
+                60: '#999999',
+                70: '#B3B3B3',
+                80: '#CCCCCC',
+                90: '#E6E6E6',
+                93: '#EDEDED',
+                95: '#F2F2F2',
+                97: '#F7F7F7',
+                100: '#ffffff',
+            },
+            Primary: {
+                main: '#329A86',
+                bgColor: '#EFF4F3',
+                Pressed: '#1C3F3A',
+                hover: '#226359',
+            },
+            Semantic : {
+              Active : "#0072DA" , 
+              Warning : "#FFAB00" , 
+              Danger : "#CC3931"
+            }
+        },
+    },
 
+    plugins: [],
+};
