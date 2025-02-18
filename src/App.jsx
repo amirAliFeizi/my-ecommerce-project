720b5cadd613b7330486ac095ebd5137548681dc
Amirali Feizi <frontify.dev@gmail.com>
2025-02-18 21:02:13 +0330

    Install React Router DOM for routing functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/pages/Home/Home";
+import Shop from "./components/pages/Shop/Shop";
+import Product from "./Components/Pages/Product/Product"
+// import ProductDetail from "./components/pages/ProductDetail";
+// import Cart from "./components/pages/Cart";
+// import Checkout from "./components/pages/Checkout";
+// import Blog from "./components/pages/Blog";
+// import About from "./components/pages/About";
+// import NotFound from "./components/pages/NotFound";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-     <h1 className='bg-red-700 text-6xl h-[200px] text-white font-bold text-center'>start to coding...</h1>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+       <Route path="/product" element={<Product/>} />
+         {/* <Route path="/cart" element={<Cart />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} /> */}
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
