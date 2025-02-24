5a4d7b85b058559898fc74c7e9520169718ba93d
developermohammadsajadjafari <developermohammadsajadjafari@gmail.com>
2025-02-24 16:37:11 +0330

    Responsive editing

--- src/Components/Pages/Product/HeaderProduct.jsx
+++ src/Components/Pages/Product/HeaderProduct.jsx
@@ -4,24 +4,20 @@ import { Location } from 'iconsax-react'
 export default function HeaderProduct() {
     return (
 
-        <section className='mx-20'>
-            <ul className="flex items-center gap-2 text-Neutral-20 text-sm sm:text-xs md:text-base">
-                <Location size="24" color="#333333" variant="TwoTone" />
-                <li className="font-IRANSansX">خانه</li>
-                <li>/</li>
-                <li className="font-IRANSansX ">ویپ | Vape</li>
-                <li>/</li>
-                <li className="font-IRANSansX">Vaporesso</li>
-                <li>/</li>
-                <li className="text-Neutral-60 font-IRANSansX truncate max-w-[200px] sm:max-w-[400px]">
+        <section className="container px-4 md:px-10 lg:px-20">
+            <ul className="flex flex-wrap items-center gap-1 text-Neutral-20 text-xs sm:text-sm md:text-base font-IRANSansX">
+                <Location size="20" className="sm:size-6" color="#333333" variant="TwoTone" />
+                <li>خانه</li>
+                <li className="before:content-['/'] before:mx-2">ویپ | Vape</li>
+                <li className="before:content-['/'] before:mx-2">Vaporesso</li>
+                <li className="before:content-['/'] before:mx-2 text-Neutral-60 truncate max-w-[150px] sm:max-w-[250px] md:max-w-[400px]">
                     ویپ آرمور مکس ویپرسو VAPORESSO ARMOUR MAX
                 </li>
             </ul>
 
-            <h1 className="text-Neutral-10 mt-6 font-IRANSansXBold text-lg sm:text-base font-bold">
+            <h1 className="text-Neutral-10 mt-4 sm:mt-6 font-IRANSansXBold text-base sm:text-lg md:text-xl font-bold">
                 ویپ آرمور مکس ویپرسو VAPORESSO ARMOUR MAX
             </h1>
-
         </section>
 
     )
