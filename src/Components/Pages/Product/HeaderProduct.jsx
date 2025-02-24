3bdd1faf6b5e4a231d906bedf363d35490c810e8
developermohammadsajadjafari <developermohammadsajadjafari@gmail.com>
2025-02-24 16:21:12 +0330

    Make it responsive

--- src/Components/Pages/Product/HeaderProduct.jsx
+++ src/Components/Pages/Product/HeaderProduct.jsx
@@ -4,7 +4,7 @@ import { Location } from 'iconsax-react'
 export default function HeaderProduct() {
     return (
 
-        <section>
+        <section className='mx-20'>
             <ul className="flex items-center gap-2 text-Neutral-20 text-sm sm:text-xs md:text-base">
                 <Location size="24" color="#333333" variant="TwoTone" />
                 <li className="font-IRANSansX">خانه</li>
@@ -14,7 +14,7 @@ export default function HeaderProduct() {
                 <li className="font-IRANSansX">Vaporesso</li>
                 <li>/</li>
                 <li className="text-Neutral-60 font-IRANSansX truncate max-w-[200px] sm:max-w-[400px]">
-                ویپ آرمور مکس ویپرسو VAPORESSO ARMOUR MAX
+                    ویپ آرمور مکس ویپرسو VAPORESSO ARMOUR MAX
                 </li>
             </ul>
 
