59015c07be8ee62094fb654e8ed66c3ccd6830f8
developermohammadsajadjafari <developermohammadsajadjafari@gmail.com>
2025-02-24 15:45:35 +0330

    Componentizing each section

--- src/Components/Pages/Product/Product.jsx
+++ src/Components/Pages/Product/Product.jsx
@@ -4,25 +4,14 @@ import ProductBig from "/images/Pic2.png"
 import ProductBig4 from "/images/image 4.png"
 import ProductBig5 from "/images/image 5.png"
 import ProductBig6 from "/images/image 6.png"
+import Navbar from './Navbar'
 
 export default function Product() {
   return (
     <>
 
-      <section>
-        <ul className='flex items-center gap-3  text-Neutral-20'>
-          <Location size="24" color="#333333" variant="TwoTone" />
-          <li className='font-IRANSansX'>خانه </li>
-          <li >/</li>
-          <li className='font-IRANSansX'>ویپ | Vape</li>
-          <li>/</li>
-          <li className='font-IRANSansX'>Vaporesso</li>
-          <li>/</li>
-          <li className='text-Neutral-60 font-IRANSansX'>ویپ آرمور مکس ویپرسو VAPORESSO ARMOUR MAX</li>
-        </ul>
-        <h1 className='text-Neutral-10 mt-8 font-IRANSansXBold text-xl font-bold'>ویپ آرمور مکس ویپرسو  VAPORESSO ARMOUR MAX</h1>
-      </section>
-      
+      <Navbar />
+
     </>
 
   )
