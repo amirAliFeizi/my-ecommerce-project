1c88decea1dabdf77792bef25e41bc73708a970a
developermohammadsajadjafari <developermohammadsajadjafari@gmail.com>
2025-02-25 13:28:29 +0330

    Editing the Action Bar Site Section

--- src/Components/Pages/Product/ActionSidebar.jsx
+++ src/Components/Pages/Product/ActionSidebar.jsx
@@ -4,8 +4,8 @@ import React from 'react'
 export default function ActionSidebar() {
     return (
 
-        <section className='container px-4 md:px-10 lg:px-20'>
-            <div className='w-14 h-[460px]  flex flex-col gap-12 mt-4 rounded-xl bg-Neutral-93 items-center justify-center'>
+        <section className='container mt-1 px-4 md:px-10 lg:px-20'>
+            <div className='w-14 h-[460px]  flex flex-col gap-12  rounded-xl bg-Neutral-93 items-center justify-center'>
                 <Heart size="24" color="#333333" />
                 <Notification size="24" color="#333333" />
                 <Share size="24" color="#333333" />
